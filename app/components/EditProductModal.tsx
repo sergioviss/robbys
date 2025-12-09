09b817696d4b6614bbed956cb5436ad36f17e833
sergioviss <sergio@cuatropuntocero.solutions>
2025-12-09 13:34:01 -0600

    Fix en modal product

--- app/components/EditProductModal.tsx
+++ app/components/EditProductModal.tsx
@@ -17,7 +17,7 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import { Separator } from "@/components/ui/separator";
+
 import { ProductType } from '../types';
 import ConfirmDialog from './ConfirmDialog';
 
